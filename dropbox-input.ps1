458bd61206e32acd4757a6891ee279bf4443039e
octochamp <49677546+octochamp@users.noreply.github.com>
2023-10-11 20:57:24 +0100

    Allowed the .ps1 scripts to overwrite files

--- dropbox-input.ps1
+++ dropbox-input.ps1
@@ -2,7 +2,7 @@
 $sourceFolder = "D:\Dropbox (BBC)\RCA IED\2023 10 Future Cities\live_link\2-ai-input-dropbox"
 $targetFolder = "\\wsl.localhost\Ubuntu\home\nathan\passive-verification-device\inputs-outputs\2-ai-input"
 
-echo "monitoring $sourceFolder and moving files to $targetFolder"
+Write-Host "monitoring $sourceFolder and moving files to $targetFolder"
 
 # Get all files with the .txt extension in the source folder
 while ($true) {
@@ -10,7 +10,7 @@ while ($true) {
 
     # Loop through each file and move it to the target folder
     foreach ($file in $files) {
-        Move-Item -Path $file.FullName -Destination $targetFolder
-        echo "moved $file"
+        Move-Item -Path $file.FullName -Destination $targetFolder -Force
+        Write-Host "moved $file"
     }
 }
